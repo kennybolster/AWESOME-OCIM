489a4805cced8015b346d7cce2c1f039f84d8403
Kenny Bolster <54642100+kennybolster@users.noreply.github.com>
2022-08-18 15:35:48 -0500

    setting up environment

---
+++ data/for_clusters/simple_model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 15:29:41 2022
+
+@author: w10146938
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+
+grid = pd.read_csv('grid_info.csv')
+#ocim1 = pd.read_csv('ocim1.csv')
+
+
