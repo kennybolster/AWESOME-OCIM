64367fda792b8dbe979ddf194d6e8815bc6c6904
Kenny Bolster <54642100+kennybolster@users.noreply.github.com>
2022-08-19 12:56:16 -0500

    starting to make new transport matrix

--- data/for_clusters/simple_model.py
+++ data/for_clusters/simple_model.py
@@ -9,9 +9,10 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from scipy import sparse
 
 grid = pd.read_csv('grid_info.csv')
-#ocim1 = pd.read_csv('ocim1.csv')
+ocim1 = pd.read_csv('ocim1.csv')
 R = 6371000
 # approximate radius of earth in meters
 grid['x'] = R * np.cos(grid.latitude/180*np.pi) * np.cos(grid.longitude/180*np.pi)
@@ -19,8 +20,12 @@ grid['y'] = R * np.cos(grid.latitude/180*np.pi) * np.sin(grid.longitude/180*np.p
 grid['z'] = R * np.sin(grid.latitude/180*np.pi)
 clustermat = grid[['depth', 'temperature', 'salinity', 'x', 'y', 'z']].values
 # making matrix for clustering algorithm
-kmn = KMeans(n_clusters = 8, random_state = 940).fit(clustermat)
+n_clusters = 8
+kmn = KMeans(n_clusters = n_clusters, random_state = 940).fit(clustermat)
 grid['cluster'] = kmn.predict(clustermat)
+
+transport = sparse.coo_matrix((n_clusters, n_clusters))
+
 
 
 
